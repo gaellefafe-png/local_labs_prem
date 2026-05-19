94df48300c28f45a5b831ac53878a95faa4dd998
gaellefafe-png <gaellefafe@gmail.com>
2026-05-19 00:53:53 -0700

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,16 @@
 import streamlit as st
 from laba_ai1 import run as run_laba_ai_1
+from laba_ai2 import run as run_laba_ai_2
 
 def main():
-    st.title("Выборо работы")
+    st.title("Выбор работы")
 
-    option = st.selectbox("Выбери лабу", ("ИИ лаба 1"))
+    option = st.selectbox("Выбери лабу", ("ИИ лаба 1", "ИИ лаба 2"))
 
     if option == "ИИ лаба 1":
         run_laba_ai_1()
+    elif option == "ИИ лаба 2":
+        run_laba_ai_2()
 
 if __name__ == "__main__":
     main()
